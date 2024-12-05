cc9b13a20b65a8064a10f04fb147ac9cc78c82fe
Lyxng69 <falikhrifqi69@gmail.com>
2024-12-05 09:06:12 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Falikh_Rifqi_Nail_Assesment_Frontend';
+}
